0eb8d3e303775c8b7f573f348e2d24ef6eda9579
Amanda Jorgensen <amanda.jorgensen.m@gmail.com>
2022-10-09 19:26:06 -0400

    added functionality

---
+++ generatedReadMe.md
@@ -0,0 +1,30 @@
+
+    # w
+
+    ## description
+    w
+
+    ## installation
+    w
+
+    ## usage
+    w
+
+    ## license
+    w
+
+    ## contributors
+    w
+
+    ## tests
+    w
+
+    ## questions
+    w
+
+    ## github
+    w
+
+    ## email
+    w
+    
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // TODO: Include packages needed for this application
 
-const { default: inquirer } = require("inquirer");
+// const { default: inquirer } = require("inquirer");
 const inquirer = require('inquirer');
 const fs = require(`fs`);
 
@@ -9,70 +9,113 @@ inquirer
         // TODO: Create an array of questions for user input
     {
     type: 'input',
-    answer: 'answer'
-    message: 'What is the title of your project?'
+    name: 'title',
+    message: 'What is the title of your project?',
     },
     {
     type: 'input',
-    answer: 'answer'
+    name: 'description',
     message: 'What is the description of your project?'
     },
     {
     type: 'input',
-    answer: 'answer'
+    name: 'installation',
     message: 'What is the installation process?'
     },
     {
     type: 'input',
-    answer: 'answer'
+    name: 'usage',
     message: 'What is the usage?'
     },
     {
     type: 'input',
-    answer: 'answer'
+    name: 'license',
     message: 'What is the license?'
     },
     {
     type: 'input',
-    answer: 'answer'
+    name: 'contributors',
     message: 'Who are the contributors?'
     },
     {
     type: 'input',
-    answer: 'answer'
+    name: 'tests',
     message: 'What tests were performed?'
     },
     {
     type: 'input',
-    answer: 'answer'
+    name: 'questions',
     message: 'Any questions or learnings?'
     },
     {
     type: 'input',
-    answer: 'answer'
+    name: 'github',
     message: 'What is your githib username?'
     },
     {
     type: 'input',
-    answer: 'answer'
+    name: 'email',
     message: 'What is your email address?'
     },
     ])
 .then (data => {
-    const filename = `$data.answer
-    .toLowerCase()
-    .split(' ')
-    .join('')}.json`;
+    console.log(data)
 
-fs.writeFile(filename, JSON.stringify(data, null, '/t'), err =>
-err ? console.log(err) : console.log(Success!)
-);
+console.log(generateReadMeTemplate(data))
+
 });
 
+function generateReadMeTemplate (data) {
+    // const template;
+    return `
+    # ${data.title}
+
+    ## description
+    ${data.description}
+
+    ## installation
+    ${data.installation}
+
+    ## usage
+    ${data.usage}
+
+    ## license
+    ${data.license}
+
+    ## contributors
+    ${data.contributors}
+
+    ## tests
+    ${data.tests}
+
+    ## questions
+    ${data.questions}
+
+    ## github
+    ${data.github}
+
+    ## email
+    ${data.email}
+    `
+}
 
 
+// Put the fs functionality inside the writetofile function, call writetofile function where the fs is right now. 
+// File name has to change.
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile("./generatedReadMe.md", generateReadMeTemplate(data), err => {
+        // err ? console.log(err) : console.log('Success!')
+        if (err) {
+            console.log(err)
+        } 
+        else {
+            console.log('Success!')
+        }
+    })
+}
+
+writeToFile();
 
 // TODO: Create a function to initialize app
 function init() {}
