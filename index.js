c785dee6fb411517ce63787b9e568ce2d3b68548
Amanda Jorgensen <amanda.jorgensen.m@gmail.com>
2022-10-09 16:00:50 -0400

    added questions object to index.js

--- index.js
+++ index.js
@@ -1,7 +1,75 @@
 // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const { default: inquirer } = require("inquirer");
+const inquirer = require('inquirer');
+const fs = require(`fs`);
+
+inquirer
+    .prompt([
+        // TODO: Create an array of questions for user input
+    {
+    type: 'input',
+    answer: 'answer'
+    message: 'What is the title of your project?'
+    },
+    {
+    type: 'input',
+    answer: 'answer'
+    message: 'What is the description of your project?'
+    },
+    {
+    type: 'input',
+    answer: 'answer'
+    message: 'What is the installation process?'
+    },
+    {
+    type: 'input',
+    answer: 'answer'
+    message: 'What is the usage?'
+    },
+    {
+    type: 'input',
+    answer: 'answer'
+    message: 'What is the license?'
+    },
+    {
+    type: 'input',
+    answer: 'answer'
+    message: 'Who are the contributors?'
+    },
+    {
+    type: 'input',
+    answer: 'answer'
+    message: 'What tests were performed?'
+    },
+    {
+    type: 'input',
+    answer: 'answer'
+    message: 'Any questions or learnings?'
+    },
+    {
+    type: 'input',
+    answer: 'answer'
+    message: 'What is your githib username?'
+    },
+    {
+    type: 'input',
+    answer: 'answer'
+    message: 'What is your email address?'
+    },
+    ])
+.then (data => {
+    const filename = `$data.answer
+    .toLowerCase()
+    .split(' ')
+    .join('')}.json`;
+
+fs.writeFile(filename, JSON.stringify(data, null, '/t'), err =>
+err ? console.log(err) : console.log(Success!)
+);
+});
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
